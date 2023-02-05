fc9d5d47c19230232ebe75d8d480a0be145a1224
cf9874 <cf9874@gmail.com>
2023-02-06 05:37:50 +0900

    asset : mock data 추가

---
+++ data/mockData.js
@@ -0,0 +1,30 @@
+export const mockData = [
+  {
+    id: 1,
+    name: "김강철",
+    Email: "cf9874@gamil.com",
+    city: "Seoul",
+    nickname: "오리",
+  },
+  {
+    id: 2,
+    name: "백대호",
+    Email: "bdh5345@naver.com",
+    city: "Jinju",
+    nickname: "장군",
+  },
+  {
+    id: 3,
+    name: "양정현",
+    Email: "neverdie223@naver.com",
+    city: "Ahnyang",
+    nickname: "주도",
+  },
+  {
+    id: 4,
+    name: "정수현",
+    Email: "wjdtngus1237@naver.com",
+    city: "Changwon",
+    nickname: "멸치",
+  },
+];
